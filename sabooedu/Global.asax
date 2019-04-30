4d69e500b2230b7396a553c9fc8df5e499a34df5
officialsolution <43287201+officialsolution@users.noreply.github.com>
2019-04-30 12:58:57 +0530

    Initial commit

---
+++ sabooedu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sabooedu.MvcApplication" Language="C#" %>
---
+++ sabooedu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sabooedu.Startup))]
+namespace sabooedu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
